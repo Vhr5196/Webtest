156ce414c839ced14dda42c87f014db67ac6e748
hemanthreddy <vankayalahemanth@gmail.com>
2020-12-26 21:34:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express=require("express");//importing express module
 const bodyParser=require("body-parser");
 const app=express();//create an express object
@@ -12,3 +13,19 @@ app.get("/",function(req,res){
     console.log("server is up and running on 3000");
 
   });
+=======
+const express=require("express");//importing express module
+const bodyParser=require("body-parser");
+const app=express();//create an express object
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
+
+  app.listen(3000,function(){
+    console.log("server is up and running on 3000");
+
+  });
+>>>>>>> 99727a8... initial commit
